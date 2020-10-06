90449c4c5f7cc1583fcf9062c6bab539a81f0716
André Patacas <guerreiroapatacas@gmail.com>
2020-10-06 15:55:45 +0100

    first class

--- tests/test_aula1.py
+++ tests/test_aula1.py
@@ -65,6 +65,14 @@ def test_separar(mock_separar):
     assert mock_separar([(1, 'a'), (2, 'b'), (3, 'c')]) == ([1,2,3], ['a','b','c'])
     assert mock_separar.call_count == 4
 
+#Exercicio 2.1
+@mock.patch('aula1.remove_e_conta', side_effect = aula1.remove_e_conta)
+def test_remove_e_conta(mock_remove_e_conta):
+    assert mock_remove_e_conta([1, 1, 2, 3, 1], 1) == ([2, 3], 3)
+    assert mock_remove_e_conta.call_count == 6
+    assert mock_remove_e_conta([1, 1, 2, 2, 3, 1], 2) == ([1, 1, 3, 1], 2)
+    assert mock_remove_e_conta.call_count == 13
+
 #Exercicio 3.3
 @mock.patch('aula1.juntar', side_effect = aula1.juntar)
 def test_juntar(mock_juntar):
